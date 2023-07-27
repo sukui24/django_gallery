dd2bb776c848de6358ce288c6a03abbe2fa9250b
sukui <diazordx@gmail.com>
2023-07-27 21:16:33 +0300

    conflicted idk why :/

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,4 @@
 from django.forms import ModelForm
-<<<<<<< HEAD
 
 from .models import ImageModel
 
@@ -7,13 +6,4 @@ from .models import ImageModel
 class ImageForm(ModelForm):
     class Meta:
         model = ImageModel
-=======
-from .models import *
-
-
-class AddImageForm(ModelForm):
-
-    class Meta:
-        model = ImageRoom
->>>>>>> 3059175a15a1141aae62dd945006ac891423502d
         fields = '__all__'
