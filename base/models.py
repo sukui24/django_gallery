f3d87ef055a5ad36032d0a99f9c93176bf731585
sukui <diazordx@gmail.com>
2023-07-31 11:28:15 +0300

    Image adding features Now images saves in specific user folder Added random numbers before actual image name to avoid unique name error * delete button shows only 'are you sure you want to delete file_name?', without random numbers prefix

--- base/models.py
+++ base/models.py
@@ -1,22 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import os
+from django.contrib import messages
+from random import randint
+
+
+def user_directory_path(instance, filename):
+
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.host.id, filename)
 
 
 class User(AbstractUser):
-    name = models.CharField(unique=True, max_length=60, null=True)
+    username = models.CharField(unique=True, max_length=60, null=True)
     email = models.EmailField(unique=True, null=True)
-    bio = models.TextField(max_length=2000, null=True)
+    bio = models.TextField(max_length=2000, null=True, blank=True)
     avatar = models.ImageField(null=True, default="avatar.svg")
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
 
 
-# TODO: if unique name - add random symbols
+# ///TODO: if unique name - add random symbols
 class ImageModel(models.Model):
     unique_name = models.CharField(max_length=100, unique=True, null=True)
     title = models.CharField(max_length=300)
-    image = models.ImageField()
+    image = models.ImageField(upload_to=user_directory_path)
 
     description = models.TextField(max_length=3000, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -26,6 +34,7 @@ class ImageModel(models.Model):
                              null=True, on_delete=models.SET_NULL)
 
     def save(self, *args, **kwargs):
+        self.image.name = str(id(randint(0, 999)))[:5] + '_' + self.image.name
         self.unique_name = os.path.basename(self.image.name)
         super().save(*args, **kwargs)
 
